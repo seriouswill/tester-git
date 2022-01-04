e27ebf28f971ec00e13a06cc0b70d2be013fec3b
Will O'Connell <williamjamesoc@gmail.com>
2022-01-04 11:35:57 +0000

    fourth

--- chatbot.py
+++ chatbot.py
@@ -9,11 +9,11 @@ from nltk.stem import WordNetLemmatizer
 from tensorflow.keras.models import load_model
 
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('.\intents.json').read())
+intents = json.loads(open('intents.json').read())
 
-words = pickle.load(open('.\words.pkl', 'rb'))
-classes = pickle.load(open('.\classes.pkl', 'rb'))
-model = load_model('.\chatbot_model.model')
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.model')
 
 
 def clean_up_sentence(sentence):
